055033f68bdb43411fa4531691efc120b4ab7c7a
Mychaela Oliveira <olindape5@gmail.com>
2025-04-01 09:21:57 -0300

    alteração  exercicio3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 nome=input("Digite o seu nome: ")
 idade=int(input("Dgite a  sua idade: "))
 salario=float(input("Qual  é o valor do seu salario: "))
-print(f"Olá {nome}, você tem {idade} anos e seu salario é {salario}")
-
-
-
+aumento=float(input("Quantos porcentos de aumento você  obteve no seu salario? "))
+valoreal=salario*aumento / 100
+novosalario = salario + valoreal
+print(f"Olá {nome}, você tem {idade} anos e seu salario é {salario}, esse mês voce recebeu {valoreal} seu salario final é de {novosalario}")
