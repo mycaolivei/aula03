a2d7deb1b614863735eece9db0fe252cabad0eb5
Mychaela Oliveira <olindape5@gmail.com>
2025-04-01 09:50:28 -0300

    envio da resolucao do atv02

---
+++ atv02.py
@@ -0,0 +1,11 @@
+n1=float(input("Digite um número: "))
+n2=float(input("Digite outro número: "))
+soma= n1 + n2
+divisao= n1 /  n2
+subtracao= n1 - n2
+multiplicacao= n1 * n2
+
+print(f"{n1} x {n2} = {multiplicacao}")
+print(f"{n1} +  {n2} = {soma}")
+print(f"{n1} - {n2} = {subtracao}")
+print(f"{n1} / {n2} = {divisao}")
