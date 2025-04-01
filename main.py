c2dae19e2c61144114d4eda7ca9819741bd8387f
Mychaela Oliveira <olindape5@gmail.com>
2025-04-01 08:54:07 -0300

    Exercicio03

---
+++ main.py
@@ -0,0 +1,7 @@
+nome=input("Digite o seu nome: ")
+idade=int(input("Dgite a  sua idade: "))
+salario=float(input("Qual  é o valor do seu salario: "))
+print(f"Olá {nome}, você tem {idade} anos e seu salario é {salario}")
+
+
+
