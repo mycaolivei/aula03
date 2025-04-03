0ba5ed970d6cb589cf87d107985ad77b11d7b851
Mychaela Oliveira <olindape5@gmail.com>
2025-04-03 09:15:53 -0300

    alteração na atv07

--- atv07.py
+++ atv07.py
@@ -2,10 +2,12 @@ tipo=input("Digite E para etanol e G para gasolina: ")
 qtdlitros=float(input("Você vai querer quantos litros?: "))
 vGas= 5.8
 vEta= 4.9
-if tipo =="G":
+if tipo == "G" or tipo == "g":
     valor = qtdlitros * vGas
-else:
+elif tipo == "E" or tipo == "e":
     valor = qtdlitros * vEta
-    print (f"Voce gastou R$ {valor:.2f}")
+else:
+    print("Tipo de combustivel invalido!")
+print(f"Voce gastou R$ {valor:.2f}")
 
 
